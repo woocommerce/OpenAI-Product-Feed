749b4cecbebaa0660e6fa49fd5932609a005de37
Naman Malhotra <naman03malhotra@gmail.com>
2025-10-09 19:23:51 +0530

    add nonce which while saving product fields

--- includes/Admin/Controllers/ProductFieldsController.php
+++ includes/Admin/Controllers/ProductFieldsController.php
@@ -402,6 +402,14 @@ class ProductFieldsController {
 	 * @param \WC_Product $product Product object being saved.
 	 */
 	public function saveProductFields( \WC_Product $product ): void {
+		if ( ! isset( $_POST['woocommerce_meta_nonce'] ) || ! wp_verify_nonce( $_POST['woocommerce_meta_nonce'], 'woocommerce_save_data' ) ) {
+			return;
+		}
+
+		if ( ! current_user_can( 'edit_products' ) ) {
+			return;
+		}
+
 		// Text fields
 		$text_keys = array(
 			'_gtin',
