f386434d9ea66fab201447f3daad538afa116ca7
Jaclyn Chen <watertranquil@gmail.com>
2025-10-21 15:14:23 +0800

    Update catalog product mapper to return compatible values.

--- includes/Platforms/OpenAI/Mappers/CatalogProductMapper.php
+++ includes/Platforms/OpenAI/Mappers/CatalogProductMapper.php
@@ -173,7 +173,7 @@ final class CatalogProductMapper implements ProductMapperInterface {
 	 *
 	 * @param \WC_Product      $product Product object.
 	 * @param \WC_Product|null $parent_product Parent product for variations.
-	 * @return int|null Parent product ID or null.
+	 * @return int Parent product ID or 0.
 	 */
 	protected function get_parent_id( \WC_Product $product, ?\WC_Product $parent_product = null ): ?int {
 		if ( $parent_product ) {
@@ -184,71 +184,178 @@ final class CatalogProductMapper implements ProductMapperInterface {
 			return $product->get_parent_id();
 		}
 
-		return null;
+		return 0;
 	}
 
 	/**
 	 * Get product images
+	 * NOTE: from WC_REST_Products_V4_Controller.get_images in core.
 	 *
 	 * @param \WC_Product      $product Product object.
 	 * @param \WC_Product|null $parent_product Parent product for fallback.
-	 * @return array Array of image URLs.
+	 * @return array Array of image objects matching WooCommerce API schema.
 	 */
 	protected function get_images( \WC_Product $product, ?\WC_Product $parent_product = null ): array {
-		$images = [];
+		$images         = array();
+		$attachment_ids = array();
 
-		// Get main image.
-		$image_id = $product->get_image_id();
-		if ( ! $image_id && $parent_product ) {
-			$image_id = $parent_product->get_image_id();
+		// Add featured image.
+		if ( $product->get_image_id() ) {
+			$attachment_ids[] = $product->get_image_id();
 		}
-		if ( $image_id ) {
-			$image_url = wp_get_attachment_url( $image_id );
-			if ( $image_url ) {
-				$images[] = $image_url;
+
+		// Add gallery images.
+		$attachment_ids = array_merge( $attachment_ids, $product->get_gallery_image_ids() );
+
+		// Build image data.
+		foreach ( $attachment_ids as $attachment_id ) {
+			$attachment_post = get_post( $attachment_id );
+			if ( is_null( $attachment_post ) ) {
+				continue;
 			}
-		}
 
-		// Get gallery images.
-		$gallery_ids = $product->get_gallery_image_ids();
-		if ( empty( $gallery_ids ) && $parent_product ) {
-			$gallery_ids = $parent_product->get_gallery_image_ids();
-		}
+			$attachment = wp_get_attachment_image_src( $attachment_id, 'full' );
 
-		foreach ( $gallery_ids as $gallery_id ) {
-			$gallery_url = wp_get_attachment_url( $gallery_id );
-			if ( $gallery_url ) {
-				$images[] = $gallery_url;
+			if ( ! is_array( $attachment ) ) {
+				continue;
 			}
+			$thumbnail = wp_get_attachment_image_src( $attachment_id, 'woocommerce_thumbnail' );
+
+			$images[] = array(
+				'id'                => (int) $attachment_id,
+				'date_created'      => wc_rest_prepare_date_response( $attachment_post->post_date, false ),
+				'date_created_gmt'  => wc_rest_prepare_date_response( strtotime( $attachment_post->post_date_gmt ) ),
+				'date_modified'     => wc_rest_prepare_date_response( $attachment_post->post_modified, false ),
+				'date_modified_gmt' => wc_rest_prepare_date_response( strtotime( $attachment_post->post_modified_gmt ) ),
+				'src'               => current( $attachment ),
+				'name'              => get_the_title( $attachment_id ),
+				'alt'               => get_post_meta( $attachment_id, '_wp_attachment_image_alt', true ),
+				'srcset'            => (string) wp_get_attachment_image_srcset( $attachment_id, 'full' ),
+				'sizes'             => (string) wp_get_attachment_image_sizes( $attachment_id, 'full' ),
+				'thumbnail'         => current( $thumbnail ),
+			);
 		}
 
 		return $images;
 	}
 
 	/**
+	 * Get image data for a single attachment
+	 *
+	 * @param int $attachment_id Attachment ID.
+	 * @param int $position Image position.
+	 * @return array|null Image data array or null if invalid.
+	 */
+	protected function get_image_data( int $attachment_id, int $position ): ?array {
+		$image_url = wp_get_attachment_url( $attachment_id );
+		if ( ! $image_url ) {
+			return null;
+		}
+
+		$attachment = get_post( $attachment_id );
+		$image_meta = wp_get_attachment_metadata( $attachment_id );
+
+		return [
+			'id'       => $attachment_id,
+			'src'      => $image_url,
+			'title'    => $attachment ? get_the_title( $attachment_id ) : '',
+			'alt'      => get_post_meta( $attachment_id, '_wp_attachment_image_alt', true ) ?: '',
+			'position' => $position,
+		];
+	}
+
+	/**
 	 * Get product attributes
+	 * NOTE: from WC_REST_Products_V1_Controller in core.
 	 *
 	 * @param \WC_Product $product Product object.
 	 * @return array Product attributes.
 	 */
 	protected function get_attributes( \WC_Product $product ): array {
-		$attributes = [];
-		$product_attributes = $product->get_attributes();
+		$attributes = array();
 
-		foreach ( $product_attributes as $key => $attribute ) {
-			if ( is_a( $attribute, 'WC_Product_Attribute' ) ) {
-				$attributes[ $key ] = [
-					'name'    => wc_attribute_label( $attribute->get_name() ),
-					'options' => $attribute->get_options(),
-					'visible' => $attribute->get_visible(),
-				];
-			} else {
-				// For simple attributes (variations).
-				$attributes[ $key ] = $attribute;
+		if ( $product->is_type( 'variation' ) ) {
+			// Variation attributes.
+			foreach ( $product->get_variation_attributes() as $attribute_name => $attribute ) {
+				$name = str_replace( 'attribute_', '', $attribute_name );
+
+				if ( ! $attribute ) {
+					continue;
+				}
+
+				// Taxonomy-based attributes are prefixed with `pa_`, otherwise simply `attribute_`.
+				if ( 0 === strpos( $attribute_name, 'attribute_pa_' ) ) {
+					$option_term = get_term_by( 'slug', $attribute, $name );
+					$attributes[] = array(
+						'id'     => wc_attribute_taxonomy_id_by_name( $name ),
+						'name'   => $this->get_attribute_taxonomy_label( $name ),
+						'option' => $option_term && ! is_wp_error( $option_term ) ? $option_term->name : $attribute,
+					);
+				} else {
+					$attributes[] = array(
+						'id'     => 0,
+						'name'   => $name,
+						'option' => $attribute,
+					);
+				}
+			}
+		} else {
+			foreach ( $product->get_attributes() as $attribute ) {
+				if ( $attribute['is_taxonomy'] ) {
+					$attributes[] = array(
+						'id'        => wc_attribute_taxonomy_id_by_name( $attribute['name'] ),
+						'name'      => $this->get_attribute_taxonomy_label( $attribute['name'] ),
+						'position'  => (int) $attribute['position'],
+						'visible'   => (bool) $attribute['is_visible'],
+						'variation' => (bool) $attribute['is_variation'],
+						'options'   => $this->get_attribute_options( $product->get_id(), $attribute ),
+					);
+				} else {
+					$attributes[] = array(
+						'id'        => 0,
+						'name'      => $attribute['name'],
+						'position'  => (int) $attribute['position'],
+						'visible'   => (bool) $attribute['is_visible'],
+						'variation' => (bool) $attribute['is_variation'],
+						'options'   => $this->get_attribute_options( $product->get_id(), $attribute ),
+					);
+				}
 			}
 		}
 
 		return $attributes;
+	}
+
+	/**
+	 * NOTE: from WC_REST_Products_V1_Controller in core.
+	 * Get attribute taxonomy label.
+	 *
+	 * @param  string $name Taxonomy name.
+	 * @return string
+	 */
+	protected function get_attribute_taxonomy_label( $name ) {
+		$tax    = get_taxonomy( $name );
+		$labels = get_taxonomy_labels( $tax );
+
+		return $labels->singular_name;
+	}
+
+	/**
+	 * NOTE: from WC_REST_Products_V1_Controller in core.
+	 * Get attribute options.
+	 *
+	 * @param int   $product_id Product ID.
+	 * @param array $attribute  Attribute data.
+	 * @return array
+	 */
+	protected function get_attribute_options( $product_id, $attribute ) {
+		if ( isset( $attribute['is_taxonomy'] ) && $attribute['is_taxonomy'] ) {
+			return wc_get_product_terms( $product_id, $attribute['name'], array( 'fields' => 'names' ) );
+		} elseif ( isset( $attribute['value'] ) ) {
+			return array_map( 'trim', explode( '|', $attribute['value'] ) );
+		}
+
+		return array();
 	}
 
 	/**
