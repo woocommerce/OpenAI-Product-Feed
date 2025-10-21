637a916d0f1ec6824cce7da110ec387f1b8e34c9
Eric Binnion <ericbinnion@gmail.com>
2025-10-21 14:32:16 -0500

    Remove unused settings repository

--- src/Admin/Controllers/AdminController.php
+++ src/Admin/Controllers/AdminController.php
@@ -9,7 +9,6 @@ declare(strict_types=1);
 
 namespace Automattic\WooCommerce\ProductFeedForOpenAI\Admin\Controllers;
 
-use Automattic\WooCommerce\ProductFeedForOpenAI\Settings\SettingsRepository;
 use Automattic\WooCommerce\ProductFeedForOpenAI\Platforms\OpenAI\FeedValidator;
 use Automattic\WooCommerce\ProductFeedForOpenAI\Admin\Helpers\CredentialValidator;
 use Automattic\WooCommerce\ProductFeedForOpenAI\Feed\ProductMapperInterface;
