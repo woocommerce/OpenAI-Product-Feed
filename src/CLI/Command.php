2ac0bbb857fd300948c6d0b88cf5503f47364af2
Radoslav Georgiev <rageorgiev@gmail.com>
2025-10-22 17:53:10 +0300

    Remove un-needed dependencies from the CLI command

--- src/CLI/Command.php
+++ src/CLI/Command.php
@@ -12,15 +12,12 @@ namespace Automattic\WooCommerce\ProductFeedForOpenAI\CLI;
 use WP_CLI;
 use WP_CLI_Command;
 use Automattic\WooCommerce\ProductFeedForOpenAI\Core\IntegrationRegistry;
-use Automattic\WooCommerce\ProductFeedForOpenAI\Feed\FeedValidatorInterface;
 use Automattic\WooCommerce\ProductFeedForOpenAI\Feed\FileBasedFeedInterface;
-use Automattic\WooCommerce\ProductFeedForOpenAI\Feed\ProductMapperInterface;
 use Automattic\WooCommerce\ProductFeedForOpenAI\Feed\ProductWalker;
 use Automattic\WooCommerce\ProductFeedForOpenAI\Feed\WalkerProgress;
 use Automattic\WooCommerce\ProductFeedForOpenAI\Platforms\OpenAI\FeedValidator;
 use Automattic\WooCommerce\ProductFeedForOpenAI\Platforms\OpenAI\ProductMapper;
 use Automattic\WooCommerce\ProductFeedForOpenAI\Settings\SettingsRepository;
-use Automattic\WooCommerce\ProductFeedForOpenAI\Storage\JsonFileFeed;
 
 /**
  * CLI command for generating a product feed.
@@ -32,20 +29,6 @@ class Command extends WP_CLI_Command {
 	 * @var IntegrationRegistry
 	 */
 	private IntegrationRegistry $integration_registry;
-
-	/**
-	 * Product mapper instance.
-	 *
-	 * @var ProductMapperInterface
-	 */
-	private ProductMapperInterface $product_mapper;
-
-	/**
-	 * Feed validator instance.
-	 *
-	 * @var FeedValidatorInterface
-	 */
-	private FeedValidatorInterface $validator;
 
 	/**
 	 * Settings repository instance.
@@ -64,13 +47,9 @@ class Command extends WP_CLI_Command {
 	 */
 	public function init(
 		IntegrationRegistry $integration_registry,
-		ProductMapper $product_mapper,
-		FeedValidator $validator,
 		SettingsRepository $settings
 	) {
 		$this->integration_registry = $integration_registry;
-		$this->product_mapper       = $product_mapper;
-		$this->validator            = $validator;
 		$this->settings             = $settings;
 	}
 
@@ -115,6 +94,11 @@ class Command extends WP_CLI_Command {
 	 */
 	public function generate( $args, $assoc_args ) {
 		// Read args and prepare defaults.
+		$timeout    = (int) $assoc_args['timeout'];
+		$batch_size = (int) $assoc_args['batch-size'];
+		$silent     = (bool) \WP_CLI\Utils\get_flag_value( $assoc_args, 'silent', false );
+		$send       = (bool) \WP_CLI\Utils\get_flag_value( $assoc_args, 'send', false );
+
 		if ( ! isset( $assoc_args['integration'] ) ) {
 			return WP_CLI::error( 'Please provide the required --integration=<integration> parameter' );
 		}
@@ -122,11 +106,6 @@ class Command extends WP_CLI_Command {
 		if ( null === $integration ) {
 			return WP_CLI::error( 'Integration not found' );
 		}
-
-		$timeout    = (int) $assoc_args['timeout'];
-		$batch_size = (int) $assoc_args['batch-size'];
-		$silent     = (bool) \WP_CLI\Utils\get_flag_value( $assoc_args, 'silent', false );
-		$send       = (bool) \WP_CLI\Utils\get_flag_value( $assoc_args, 'send', false );
 
 		// Verify settings in advance if there is a requirement to send the feed.
 		$endpoint = null;
